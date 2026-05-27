27769cc506b8b8950b7c354100d00985a54baab1
nsalman <nsalman@griddynamics.com>
2026-05-27 12:18:57 +0530

    initialize project structure

---
+++ src/main/java/com/talentgrid/TalentGridParentApplication.java
@@ -0,0 +1,13 @@
+package com.talentgrid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TalentGridParentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TalentGridParentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/talentgrid/TalentgridParentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.talentgrid;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TalentgridParentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
