7a20f6a98667abcc7ab0e92ba78948b43823e647
awankhadegrid <awankhade@griddynamics.com>
2026-06-03 13:33:01 +0530

    initial project setup for team 5

---
+++ services/workforce-service/src/main/java/com/talentgrid/workforce/WorkforceServiceApplication.java
@@ -0,0 +1,13 @@
+package com.talentgrid.workforce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkforceServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WorkforceServiceApplication.class, args);
+        System.out.println("testing main method");
+    }
+}
