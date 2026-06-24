ee39a2ad9383d26bdd473e0e5b6a57f4e519635b
arameshgrid <aramesh@griddynamics.com>
2026-06-24 21:42:21 +0530

    Update SecurityConfig.java

--- services/user-auth-service/src/main/java/com/talentgrid/auth/config/SecurityConfig.java
+++ services/user-auth-service/src/main/java/com/talentgrid/auth/config/SecurityConfig.java
@@ -12,8 +12,6 @@ import org.springframework.security.config.annotation.authentication.configurati
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
@@ -71,11 +69,6 @@ public class SecurityConfig {
                                                 UsernamePasswordAuthenticationFilter.class);
 
                 return http.build();
-        }
-
-        @Bean
-        public PasswordEncoder passwordEncoder() {
-                return new BCryptPasswordEncoder();
         }
 
         @Bean
